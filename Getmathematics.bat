5c4098c0879b3108cd70278dddf313781e7b448f
mahone-xp <mayanfeng_xd@outlook.com>
2019-04-19 18:25:02 +0800

    Update Getmathematics.bat
    修改一些备注

--- Getmathematics.bat
+++ Getmathematics.bat
@@ -4,7 +4,6 @@ REM chcp 65001 强制转换为utf-8编码格式显示
 mode con cols=49 lines=10
 color 0A
 cls
-REM QuestQTY:题目数量 TaskRange:数值上限
 echo.
 echo.
 echo.
@@ -14,8 +13,7 @@ echo.
 set /p TaskRange=请输入题目结果最大值：
 cls
 echo 请稍等，%QuestQTY%道%TaskRange%以内加减法正在生成中……
-REM 启用延迟环境变量扩展
-REM 一直不知道为什么要加上这个才行
+REM 启用延迟环境变量扩展,应该跟c中的生成种子类似
 setlocal enabledelayedexpansion
 rem 归零计数器
 set /a CreateNum=0
@@ -25,11 +23,9 @@ echo %TaskRange%以内加减法%QuestQTY%道 >math.txt
 :CreateMath
 set /a MSymbol=!random!%%2
 if %CreateNum% LSS %QuestQTY% (
-set /a CreateNum=CreateNum+1    
-REM CreateNum+1 记录增加了一次计算
+set /a CreateNum=CreateNum+1
 if %MSymbol% == 1 (
 set /a PlusNum=PlusNum+1
-rem 判断 MSymbol 的值，若为 “1” 则生成加法算式
 goto :PlusCal ) else (
 set /a MinusNum=MinusNum+1
 goto :MinusCal)) else (
@@ -42,6 +38,7 @@ set /a Nums2=!random!%%TaskRange+1
 set /a NumSum=Nums1+Nums2
 if !NumSum! LEQ %TaskRange% (
 echo !Nums1! + !Nums2! =>> math.txt) else goto :PlusCal
+REM 判断和值大于阈值，goto:PlusCal
 goto :CreateMath
 :MinusCal
 set /a Nums1=!random!%%TaskRange+1
@@ -49,6 +46,7 @@ set /a Nums2=!random!%%TaskRange+1
 if !Nums1! GEQ !Nums2! (
 echo !Nums1! - !Nums2! =>> math.txt) else (
 echo !Nums2! - !Nums1! =>> math.txt )
+REM 减法计算比较简单，大的做被减数，小的做减数
 goto :CreateMath
 :end
 cls
